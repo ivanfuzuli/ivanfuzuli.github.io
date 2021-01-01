5771912fdb694579b8066b6a3f0b93e7593e3c09
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2021-01-01 22:53:56 +0300

    manifest updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Can Küçükyılmaz'ın Blogu`,
+        short_name: `Can Küçükyılmaz`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
